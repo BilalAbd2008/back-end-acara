9ec061a09301a72f42b73a59ecb755c5332d9ea0
BilalAbd2008 <abdurrohmanbilaol.2008@gmail.com>
2025-07-08 06:35:19 +0700

    test-acl

--- src/middlewares/acl.middleware.ts
+++ src/middlewares/acl.middleware.ts
@@ -3,15 +3,22 @@ import { IReqUser } from "./auth.middleware";
 
 export default (roles: string[]) => {
   return (req: IReqUser, res: Response, next: NextFunction) => {
-    const role = req.user?.role;
-
-    if (!role || !roles.includes(role)) {
+    // Jika belum login, kembalikan Unauthorized
+    if (!req.user) {
       return res.status(401).json({
+        data: null,
+        message: "Unauthorized",
+      });
+    }
+
+    const role = req.user.role;
+    // Jika role tidak sesuai, kembalikan Forbidden
+    if (!roles.includes(role)) {
+      return res.status(403).json({
         data: null,
         message: "Forbidden",
       });
     }
-
     next();
   };
 };
